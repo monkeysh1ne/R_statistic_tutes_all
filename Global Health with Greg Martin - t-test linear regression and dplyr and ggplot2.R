0ae4a871c8eb4731888d85b9d6dd4bf4a0488db5
monkeysh1ne <mcdoob@gmail.com>
2019-09-17 15:58:42 +1200

    Load and start using DPLYR package in Global Health w/ Greg Martin - t-test linear regression etc tutorial

--- Global Health with Greg Martin - t-test linear regression and dplyr and ggplot2.R
+++ Global Health with Greg Martin - t-test linear regression and dplyr and ggplot2.R
@@ -16,3 +16,15 @@ hist(lifeExp)
 hist(log(pop))
 boxplot(lifeExp ~ continent, xlab="continent", ylab="age")
 plot(lifeExp ~ log(gdpPercap))     
+
+
+install.packages("dplyr")
+library(dplyr) # gives us access to pipe operator amongst other things
+
+gapminder %>% 
+        select(country, lifeExp) %>% 
+        filter(country == "South Africa" |
+                       country == "Ireland") %>% 
+        group_by(country) %>% 
+        summarise(Average_life = mean(lifeExp))
+        
